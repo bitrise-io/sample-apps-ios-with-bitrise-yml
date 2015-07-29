1206f41122b110f9534acd8ee491748af4c9afd8
Viktor Benei <viktor.benei@gmail.com>
2015-07-29 15:48:59 +0200

    base bitrise.yml with test, analyze & archive

---
+++ install_bitrise_cli.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -v
+set -e
+
+curl -L https://github.com/bitrise-io/bitrise-cli/releases/download/0.9.5/bitrise-cli-$(uname -s)-$(uname -m) > /usr/local/bin/bitrise-cli
+
+chmod +x /usr/local/bin/bitrise-cli
+
+bitrise-cli setup
