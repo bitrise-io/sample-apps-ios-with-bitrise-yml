4f18bc3c046f09462ca05fd7255234fabfd838b1
Viktor Benei <viktor.benei@gmail.com>
2015-08-04 17:44:39 +0200

    updated for bitwise-cli 0.9.6 + included the new (beta) certificate downloader

--- install_bitrise_cli.sh
+++ install_bitrise_cli.sh
@@ -3,7 +3,7 @@
 set -v
 set -e
 
-curl -L https://github.com/bitrise-io/bitrise-cli/releases/download/0.9.5/bitrise-cli-$(uname -s)-$(uname -m) > /usr/local/bin/bitrise-cli
+curl -L https://github.com/bitrise-io/bitrise-cli/releases/download/0.9.6/bitrise-cli-$(uname -s)-$(uname -m) > /usr/local/bin/bitrise-cli
 
 chmod +x /usr/local/bin/bitrise-cli
 
